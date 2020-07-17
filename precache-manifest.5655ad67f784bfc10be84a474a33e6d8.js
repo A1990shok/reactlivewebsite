c6b37618e13955144577f86eaf90aff535a9df6d
ashok kumar <ashokyadavaky11@gmail.com>
2020-07-17 15:24:14 +0530

    Updates

---
+++ precache-manifest.5655ad67f784bfc10be84a474a33e6d8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3fd3e56d807e1ffeb673f07cbae1391",
+    "url": "/reactlivewebsite/index.html"
+  },
+  {
+    "revision": "8bc5045da31ddcbfd5f7",
+    "url": "/reactlivewebsite/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "b8111344bcef4462effe",
+    "url": "/reactlivewebsite/static/css/main.95f26d6b.chunk.css"
+  },
+  {
+    "revision": "8bc5045da31ddcbfd5f7",
+    "url": "/reactlivewebsite/static/js/2.bf49de38.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactlivewebsite/static/js/2.bf49de38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8111344bcef4462effe",
+    "url": "/reactlivewebsite/static/js/main.0207076d.chunk.js"
+  },
+  {
+    "revision": "d98d34a2995e32d3695a",
+    "url": "/reactlivewebsite/static/js/runtime-main.250453a5.js"
+  },
+  {
+    "revision": "34dbd26613205d519d21972643913f94",
+    "url": "/reactlivewebsite/static/media/an2.34dbd266.png"
+  },
+  {
+    "revision": "80b09c89d76b9699a53c9024f969f00a",
+    "url": "/reactlivewebsite/static/media/ap.80b09c89.jpg"
+  },
+  {
+    "revision": "83366726712fbea44bb3adeb74e372fc",
+    "url": "/reactlivewebsite/static/media/develo.83366726.png"
+  },
+  {
+    "revision": "f282a3277e133bc12da201e22d90ac52",
+    "url": "/reactlivewebsite/static/media/dmarketing.f282a327.png"
+  },
+  {
+    "revision": "beefeac16fddad3d61ab49839305f1f3",
+    "url": "/reactlivewebsite/static/media/h2.beefeac1.png"
+  },
+  {
+    "revision": "91ddd5fc5465fba753dfa5df7bd631bb",
+    "url": "/reactlivewebsite/static/media/img.91ddd5fc.jpg"
+  },
+  {
+    "revision": "3af00ed46f318dd07506776812576f61",
+    "url": "/reactlivewebsite/static/media/m2.3af00ed4.jpeg"
+  },
+  {
+    "revision": "2e98e6afecc9a54213ac717563762ea9",
+    "url": "/reactlivewebsite/static/media/software.2e98e6af.jpg"
+  },
+  {
+    "revision": "a511e2ed50c958bea9cdbde0fa7bb162",
+    "url": "/reactlivewebsite/static/media/sup1.a511e2ed.png"
+  }
+]);
