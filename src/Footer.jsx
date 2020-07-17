89bbf0c1d0dff3c9c4789e66db21a569cd19cbb6
ashok kumar <ashokyadavaky11@gmail.com>
2020-07-17 15:05:27 +0530

    my first commit reactwebsite

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="bg-light text-center footer-f1">
+        <p>© 2020 ProxyKhel. All Right Reserved | Terms and condition </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/Sdata.jsx
@@ -0,0 +1,35 @@
+import web from "../src/Images/develo.png";
+import app from "../src/Images/ap.jpg";
+import android from "../src/Images/an2.png";
+import digital from "../src/Images/dmarketing.png";
+import marketin from "../src/Images/m2.jpeg";
+import software from "../src/Images/software.jpg";
+
+const Sdata = [
+  {
+    imgsrc: web,
+    title: "Web development",
+  },
+  {
+    imgsrc: app,
+    title: "App development",
+  },
+  {
+    imgsrc: software,
+    title: "Software development",
+  },
+  {
+    imgsrc: digital,
+    title: "Digital marketing",
+  },
+  {
+    imgsrc: android,
+    title: "Android development",
+  },
+  {
+    imgsrc: marketin,
+    title: "Marketing ",
+  },
+];
+
+export default Sdata;
